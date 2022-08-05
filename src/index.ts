d0d9775997b9064296792b65fbc8e5ca17ecb42a
vinnidias <vinicius.3991@gmail.com>
2022-08-05 18:44:56 -0300

    first route on app

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import express from "express";
 
+const createTask = require("./routes/createTask") 
+
 const app = express();
 
 const port = 3330
 
 app.listen(port, ()=> console.log(`server is runnig on port: ${port}`))
+
+app.use(express.json());
+
+app.use("/tasks", createTask)
