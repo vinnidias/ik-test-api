c7c50129cd513536c66b1877bca4c96a37b3b321
vinnidias <vinicius.3991@gmail.com>
2022-08-06 09:12:05 -0300

    DTOS created to useCases

---
+++ src/dtos/CreateTaskDTO.ts
@@ -0,0 +1,7 @@
+export interface CreateTaskDTO {
+  name: string;
+  title: string; 
+  description: string; 
+  date: string; 
+  hour: string;
+}
---
+++ src/dtos/UpdateTaskDTO.ts
@@ -0,0 +1,8 @@
+export interface UpdateTaskDTO {
+  id: string;
+  name?: string;
+  title?: string; 
+  description?: string; 
+  date?: string; 
+  hour?: string;
+}
