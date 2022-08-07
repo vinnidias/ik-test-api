7d04bd408688d00e1f8af66e9867d0d00eb71c42
vinnidias <vinicius.3991@gmail.com>
2022-08-06 21:38:00 -0300

    correcting uppercase var

--- src/useCases/getAllTasks/GetAllTasksController.ts
+++ src/useCases/getAllTasks/GetAllTasksController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 
-import { GetAllTasksUseCase } from "./getAllTasksUseCase";
+import { GetAllTasksUseCase } from "./GetAllTasksUseCase";
 
 export class GetAllTasksController {
   async handle(req: Request, res: Response) {
