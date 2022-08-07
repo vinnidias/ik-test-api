ecd860f9e60930df5cce796e93722bdcf8a4d5fe
vinnidias <vinicius.3991@gmail.com>
2022-08-06 21:28:15 -0300

    cors middleware config

--- src/index.ts
+++ src/index.ts
@@ -9,24 +9,22 @@ const app = express();
 
 const port = 3330;
 
-const corsOptions ={
-  origin:'*', 
-  credentials:true,            //access-control-allow-credentials:true
-  optionSuccessStatus:200
-}
+const corsOptions = {
+  origin: "*",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  optionSuccessStatus: 200,
+};
 
 app.use(express.json());
 
-app.use("/", routes);
+app.use("/", cors(corsOptions), routes);
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST,DELETE");
+  res.header("Access-Control-Allow-Origin", "http://localhost:3001");
+  res.header("Access-Control-Allow-Methods", "GET, PATCH, POST,DELETE");
 
   next();
 });
-
-
 
 app.use(
   (err: Error, request: Request, response: Response, next: NextFunction) => {
@@ -43,7 +41,5 @@ app.use(
     });
   }
 );
-
-app.use(cors(corsOptions));
 
 app.listen(port, () => console.log(`server is runnig on port: ${port}`));
